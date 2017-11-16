42bc764517049b26159f11b3b2ebdbfe1dd5e045
Juan E Rolon <rolon.math@gmail.com>
2017-11-16 12:19:40 -0600

    updated datasets loading

--- dataset-load.py
+++ dataset-load.py
@@ -5,15 +5,18 @@ import pandas as pd
 
 demographics_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/demographics/'
 smoking_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/smoking/'
+alcohol_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/alcohol_use/'
 
 
 #datafiles are in SAS format
 demographics_data = pd.read_sas(demographics_path + 'DEMO_H.XPT')
 smoking_data = pd.read_sas(smoking_path + 'SMQ_H.XPT')
+alcohol_data = pd.read_sas(alcohol_path + 'ALQ_H.XPT')
 
 #datasets keys or column fields
 demographics_vars = list(demographics_data.keys())
-smoking_vars = list(smoking_data.keys())
+smoking_vars =  list(smoking_data.keys())
+alcohol_vars = list(alcohol_data.keys())
 
 #dataset quick preview of a subset of the column fields (variables of interest)
 print("\nDemographics data exploration:\n")
@@ -21,3 +24,6 @@ print(demographics_data[['SEQN', 'RIDAGEYR', 'RIAGENDR', 'INDHHIN2']].head())
 
 print("\nSmoking cigarettes use data exploration:\n")
 print(smoking_data[['SMQ040', 'SMQ020']].head())
+
+print("\nAlcohol use data exploration:\n")
+print(alcohol_data[['ALQ120Q', 'ALQ130']].head())
