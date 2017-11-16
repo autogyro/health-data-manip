a201b1c64f2521f7ff3cf3547801977239f61b87
Juan E Rolon <rolon.math@gmail.com>
2017-11-16 13:06:20 -0600

    cleaned filepaths text

--- dataset-load.py
+++ dataset-load.py
@@ -3,21 +3,16 @@
 
 import pandas as pd
 
-demographics_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/demographics/'
-smoking_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/smoking/'
-alcohol_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/alcohol_use/'
-weight_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/weight_history/'
-physical_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/physical_activity/'
-pressurechol_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/blood_pressure/'
-
+datasets_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/'
 
 #datafiles are in SAS format
-demographics_data = pd.read_sas(demographics_path + 'DEMO_H.XPT')
-smoking_data = pd.read_sas(smoking_path + 'SMQ_H.XPT')
-alcohol_data = pd.read_sas(alcohol_path + 'ALQ_H.XPT')
-weight_data = pd.read_sas(weight_path + 'WHQ_H.XPT')
-physical_data = pd.read_sas(physical_path + 'PAQ_H.XPT')
-pressurechol_data = pd.read_sas(pressurechol_path + 'BPQ_H.XPT')
+demographics_data = pd.read_sas(datasets_path + 'demographics/DEMO_H.XPT')
+smoking_data = pd.read_sas(datasets_path + 'smoking/SMQ_H.XPT')
+alcohol_data = pd.read_sas(datasets_path + 'alcohol_use/ALQ_H.XPT')
+weight_data = pd.read_sas(datasets_path + 'weight_history/WHQ_H.XPT')
+physical_data = pd.read_sas(datasets_path + 'physical_activity/PAQ_H.XPT')
+pressurechol_data = pd.read_sas(datasets_path + 'blood_pressure/BPQ_H.XPT')
+nutrition_data = pd.read_sas(datasets_path + 'diet_nutrition/DBQ_H.XPT')
 
 #datasets keys or column fields
 demographics_vars = list(demographics_data.keys())
@@ -26,6 +21,7 @@ alcohol_vars = list(alcohol_data.keys())
 weight_vars = list(alcohol_data.keys())
 physical_vars = list(physical_data.keys())
 pressurechol_vars = list(pressurechol_data.keys())
+nutrition_vars = list(nutrition_data.keys())
 
 #datasets' quick preview of a subset of the column fields (variables of interest)
 print("\nDemographics data exploration:\n")
@@ -45,3 +41,6 @@ print(physical_data[['SEQN','PAQ650', 'PAQ655']].head())
 
 print("\nBlood pressure & cholesterol data exploration:\n")
 print(pressurechol_data[['SEQN','BPQ020','BPQ040A', 'BPQ050A','BPQ080', 'BPQ090D']].head())
+
+print("\nDiet and nutrition data exploration:\n")
+print(nutrition_data[['SEQN','DBD895','DBD900', 'DBD905','DBD910']].head())
