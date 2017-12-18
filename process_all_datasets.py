2d3f047c42aded4d8c0df89dc484a71546d7012b
Juan E Rolon <rolon.math@gmail.com>
2017-12-18 11:11:38 -0600

    fixed demographics data processing

--- process_all_datasets.py
+++ process_all_datasets.py
@@ -24,21 +24,32 @@ project_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/cap
 ###############################
 print("")
 demographics_data = pd.read_sas(datasets_path + 'demographics/DEMO_H.XPT')
-
-ager_data = txt.restrict_by_interval(demographics_data, 'RIDAGEYR', 18, 65, 'inclusive')
-ager_data = txt.switch_df_index(ager_data, 'SEQN')
+demographics_data = txt.switch_df_index(demographics_data, 'SEQN')
+demographics_data = demographics_data[['RIDAGEYR', 'RIAGENDR','RIDRETH3','INDHHIN2']]
 #Restrict dataframe to selected columns
-ager_data = ager_data[['RIDAGEYR', 'RIAGENDR','INDHHIN2']]
+demographics_data = txt.restrict_by_interval(demographics_data, 'RIDAGEYR', 18, 65, 'inclusive')
 #Rename original columns to user specified names
-ager_data.rename(columns = {'RIDAGEYR':'AGE', 'RIAGENDR':'GENDER', 'INDHHIN2':'INCOME_LEVEL'}, inplace=True)
-#Remove records (rows) with NaNs, and those containing 77.0 ('Refused') or 99 ('Unknown')
-ager_data.dropna(axis=0, how='any', inplace=True)
-ager_data = ager_data[ager_data.INCOME_LEVEL != 77.0]
-ager_data = ager_data[ager_data.INCOME_LEVEL != 99.0]
+demographics_data.rename(columns = {'RIDAGEYR':'AGE', 'RIAGENDR':'GENDER',
+                            'RIDRETH3':'ETHNICITY','INDHHIN2':'INCOME_LEVEL'}, inplace=True)
+
+demo_features = ['AGE','GENDER','ETHNICITY','INCOME_LEVEL']
+
+demographics_data.INCOME_LEVEL.fillna(value=7, inplace=True)
+for val in [77.0,99.0]:
+    demographics_data['INCOME_LEVEL'].replace(to_replace=val, value=7, inplace=True)
 #Downcast selected features
-ager_data['AGE'] = pd.to_numeric(ager_data['AGE'], downcast='integer')
-ager_data['GENDER'] = pd.to_numeric(ager_data['GENDER'], downcast='integer')
-ager_data['INCOME_LEVEL'] = pd.to_numeric(ager_data['INCOME_LEVEL'], downcast='integer')
+for feat in demo_features:
+    demographics_data[feat] = pd.to_numeric(demographics_data[feat], downcast='integer')
+
+
+txt.get_feature_counts(demographics_data,demo_features)
+txt.count_feature_nans(demographics_data, demo_features)
+
+
+sys.exit()
+
+
+
 
 txt.headcounts(ager_data)
 
