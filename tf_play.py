1fd328ffec3fdfb721f90fa39c5dfa2860f2a594
Juan E Rolon <rolon.math@gmail.com>
2018-02-10 21:52:53 -0600

    start keras default session

--- tf_play.py
+++ tf_play.py
@@ -1,6 +1,67 @@
 
-
+import numpy as np
 import tensorflow as tf
+
+
+from sklearn.metrics import roc_curve, auc
+
+labels = np.array([0, 0, 1, 1])
+preds = np.array([0.1, 0.4, 0.35, 0.8])
+
+def tflow_auc(labels, preds):
+    auc, update_op = tf.metrics.auc(labels, preds)
+    with tf.Session() as sess:
+        sess.run(tf.local_variables_initializer())
+        #print("tf auc: {}".format(sess.run([auc, update_op])))
+        return sess.run([auc, update_op])
+
+print(tflow_auc(labels,preds))
+print(type(tflow_auc(labels,preds)))
+
+def k_custom_auc(y_true, y_pred):
+    fpr, tpr, dash = roc_curve(y_true, y_pred)
+    score = tf.constant(auc(fpr, tpr))
+    sess = tf.Session()
+    return sess.run(score)
+
+
+def k_custom_auc2(y_true, y_pred):
+    fpr, tpr, dash = roc_curve(y_true, y_pred)
+    score = tf.constant(auc(fpr, tpr))
+
+    return score
+
+def k_custom_auc3(y_true, y_pred):
+    fpr, tpr, dash = roc_curve(y_true, y_pred)
+    score = tf.Variable(auc(fpr, tpr))
+
+    return score
+
+
+print(k_custom_auc(labels,preds))
+print(type(k_custom_auc(labels,preds)))
+
+print(k_custom_auc2(labels,preds))
+print(type(k_custom_auc2(labels,preds)))
+
+print(k_custom_auc3(labels,preds))
+print(type(k_custom_auc3(labels,preds)))
+
+
+import keras.backend as K
+
+def mean_pred(y_pred):
+    y_pred = K.variable(y_pred)
+    return K.mean(y_pred)
+
+sess = K.get_session()
+with sess.as_default():
+    print(mean_pred(labels))
+
+
+
+
+
 
 #Initial tests
 if False:
