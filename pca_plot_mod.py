fd864dcf3973a796d05ecf15ba4b4d138f2f31c1
Juan E Rolon <rolon.math@gmail.com>
2018-03-06 14:43:41 -0600

    initial upload

---
+++ pca_plot_mod.py
@@ -0,0 +1,70 @@
+
+import sys
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#import local modules
+from importlib.machinery import SourceFileLoader
+mod_path = "/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/nhanes2013-2104/"
+txt = SourceFileLoader("text_utils", mod_path+"text_utils.py").load_module()
+gut = SourceFileLoader("graph_utils", mod_path+"graph_utils.py").load_module()
+
+
+#Path to datasets folders
+
+datasets_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/'
+project_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/'
+
+full_data = pd.read_csv(project_path + 'nhanes_2013_2014_full_data.csv',index_col=0)
+
+data_cols = list(full_data.columns)
+bio_cols = []
+que_cols = []
+
+for col in data_cols:
+    if ('LBX' in col):
+        bio_cols.append(col)
+    else:
+        que_cols.append(col)
+
+biochemistry_data = full_data[bio_cols]
+questionnaire_data = full_data[que_cols]
+
+
+
+def pca_results(good_data, pca):
+	'''
+	Constructs barchart plots of principal components analysis of
+	the input dataframe "good_data" given the input pca object "pca".
+	'''
+
+	# Dimension indexing
+	dimensions = dimensions = ['Dimension {}'.format(i) for i in range(1,len(pca.components_)+1)]
+
+	# PCA components
+	components = pd.DataFrame(np.round(pca.components_, 4), columns = good_data.keys())
+	components.index = dimensions
+
+	# PCA explained variance
+	ratios = pca.explained_variance_ratio_.reshape(len(pca.components_), 1)
+	variance_ratios = pd.DataFrame(np.round(ratios, 4), columns = ['Explained Variance'])
+	variance_ratios.index = dimensions
+
+	# Create a bar plot visualization
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Plot the feature weights as a function of the components
+	components.plot(ax = ax, kind = 'bar');
+	ax.set_ylabel("Feature Weights")
+	ax.set_xticklabels(dimensions, rotation=0)
+
+
+	# Display the explained variance ratios
+	for i, ev in enumerate(pca.explained_variance_ratio_):
+		ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f"%(ev))
+
+	# Return a concatenated DataFrame
+	return pd.concat([variance_ratios, components], axis = 1)
