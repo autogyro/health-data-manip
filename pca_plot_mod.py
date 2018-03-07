61d37265a39991ac84cf0b19bffdab511d966d39
Juan E Rolon <rolon.math@gmail.com>
2018-03-06 21:02:13 -0600

    fixed var ratios

--- pca_plot_mod.py
+++ pca_plot_mod.py
@@ -30,6 +30,10 @@ for col in data_cols:
     else:
         que_cols.append(col)
 
+print("Num. features in biochemistry data: {}".format(len(bio_cols)))
+print("Num. features in questionnaire data: {}".format(len(que_cols)))
+
+
 biochemistry_data = full_data[bio_cols]
 questionnaire_data = full_data[que_cols]
 
@@ -46,23 +50,53 @@ dimensions = dimensions = ['Dimension {}'.format(i) for i in range(1, len(pca.co
 components = pd.DataFrame(np.round(pca.components_, 4), columns=good_data.keys())
 components.index = dimensions
 
-print("\nDimensions\n {}".format(dimensions))
-print("\nComponents DF\n {}".format(components))
-
-
-subcomponets_1 = components.loc[components.index[[0, 1]]]
-print("\nSubcomponents1 DF\n {}".format(subcomponets_1))
-
 
 # PCA explained variance
-ratios = pca.explained_variance_ratio_.reshape(len(pca.components_), 1)
+bare_ratios = pca.explained_variance_ratio_
+ratios = bare_ratios.reshape(len(pca.components_), 1)
+
 variance_ratios = pd.DataFrame(np.round(ratios, 4), columns=['Explained Variance'])
 variance_ratios.index = dimensions
 
 print("\nBare Explained Variance Ratios\n {}".format(pca.explained_variance_ratio_))
-
 print("\nExplained Variance Ratios DF\n {}".format(variance_ratios))
 
+print("\nDimensions\n {}".format(dimensions))
+print("\nComponents DF\n {}".format(components))
+
+nplots = 3
+sub_components = []
+sub_dimensions = []
+sub_var_ratios = []
+
+#plt.figure(1, figsize=(12, 6))
+
+for i in range(nplots):
+    fig, ax = plt.subplots(figsize=(14, 8))
+    sc = components.loc[components.index[2*i:2*(i+1)]]
+    vr = bare_ratios[2 * i:2 * (i + 1)]
+    ix = sc.index
+
+    sub_components.append(sc)
+    sub_var_ratios.append(vr)
+    sub_dimensions.append(ix)
+
+    if False:
+        print("\nSubcomponent DF[{}]:\n {}".format(i,sc))
+        print("\nSub_var_ratios[{}]:\n {}".format(i,vr))
+        print("\nDF Subdimensions[{}]:\n {}".format(i, ix))
+
+    sc.plot(ax=ax, kind='bar');
+    ax.set_ylabel("Feature Weights")
+    ax.set_xticklabels(sc.index, rotation=0)
+
+    for j, ev in enumerate(vr):
+        ax.text(j - 0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n %.4f" % (ev))
+
+
+plt.show()
+
+sys.exit()
 # Create a bar plot visualization
 fig, ax = plt.subplots(figsize=(14, 8))
 
@@ -73,7 +107,7 @@ ax.set_xticklabels(dimensions, rotation=0)
 
 # Display the explained variance ratios
 for i, ev in enumerate(pca.explained_variance_ratio_):
-    ax.text(i - 0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f" % (ev))
+    ax.text(i - 0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n %.4f" % (ev))
 
 # Return a concatenated DataFrame
 pca_results_df =  pd.concat([variance_ratios, components], axis=1)
@@ -81,3 +115,26 @@ print("\nPCA Results DF\n")
 print(pca_results_df)
 
 plt.show()
+
+if False:
+
+    plt.figure(1, figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
+    sns.set(font_scale=1.2)
+    heat_map = sns.heatmap(pca_matrix, cbar=True, annot=True, square=True, fmt='.4f',
+                           annot_kws={'size': 12}, yticklabels=rows, xticklabels=cols)
+    plt.title("Feature loadings correlation matrix")
+    plt.xticks(rotation='vertical')
+    plt.yticks(rotation='horizontal')
+
+    plt.subplot(1, 2, 2)
+    sns.set(font_scale=1.2)
+    heat_map = sns.heatmap(pca_squared_matrix, cmap="YlGnBu", cbar=True, annot=True, square=True, fmt='.4f',
+                           annot_kws={'size': 12}, yticklabels=rows, xticklabels=cols)
+    plt.title("Variance percentages explained by feature")
+    plt.xticks(rotation='vertical')
+    plt.yticks(rotation='horizontal')
+
+    plt.tight_layout()
+    plt.show()
