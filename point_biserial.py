a188aeb49667aca5d6414c0bcaeef781f34ed957
Juan E Rolon <rolon.math@gmail.com>
2018-03-10 19:26:48 -0600

    added corr questionnaire biochem correlation

--- point_biserial.py
+++ point_biserial.py
@@ -66,7 +66,7 @@ if False:
 
     from scipy.stats import pointbiserialr
     print("\nPoint biserial corr coeff:")
-    print(pointbiserialr(biochemistry_data.LBXSTR,questionnaire_data.HYPERTENSION))
+    print(pointbiserialr(biochemistry_data.LBXSTR,questionnaire_data.HYPERTENSION)[0])
 
 
 
@@ -86,7 +86,7 @@ if False:
         print("Energy Distance = {}\n".format(dcor.energy_distance(biochemistry_data[feat], questionnaire_data["HYPERTENSION"])))
 
 
-#Plot corr matrix heatmap
+#Plot correlation matrix using Pearson's correlation measure
 if False:
     import seaborn as sns
     cols = list(biochemistry_data.columns)
@@ -102,12 +102,41 @@ if False:
     plt.show()
 
 
-import dcor # E-statistics module
-import seaborn as sns
-cols = list(biochemistry_data.columns)
+#Plot correlation matrix using Spearman's correlation measure
+if False:
+    from scipy.stats import spearmanr
+    import seaborn as sns
 
-#version 2
-if True:
+    cols = list(biochemistry_data.columns)
+
+    container = len(cols)*len(cols)*[0.0]
+    for i in range(len(cols)):
+        for j in range(i, len(cols)):
+            m = i * len(cols) + j
+            n = j * len(cols) + i
+            container[m] = spearmanr(biochemistry_data[cols[i]], biochemistry_data[cols[j]])
+            container[n] = container[m]
+
+    spearmans_corr_matrix = np.reshape(container, (-1, len(cols)))
+
+    plt.figure(1, figsize=(12, 18))
+    sns.set(font_scale=1.0)
+    heat_map = sns.heatmap(spearmans_corr_matrix, cbar=False, annot=True, square=True, fmt='.2f',
+                   annot_kws = {'size': 10}, yticklabels=cols, xticklabels=cols)
+    plt.xticks(rotation='vertical')
+    plt.yticks(rotation='horizontal')
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+#Plot correlation matrix using distance correlation measure
+if False:
+    import dcor  # E-statistics module
+    import seaborn as sns
+
+    cols = list(biochemistry_data.columns)
 
     container = len(cols)*len(cols)*[0.0]
     for i in range(len(cols)):
@@ -128,21 +157,41 @@ if True:
     plt.tight_layout()
     plt.show()
 
-#version 1
-if False:
-    distance_corr_matrix = np.zeros((len(cols), len(cols)))
-    container = []
-    for i, col_i in enumerate(cols):
-        for j, col_j in enumerate(cols):
-            container.append(dcor.distance_correlation(biochemistry_data[col_i], biochemistry_data[col_j]))
 
 
-    distance_corr_matrix = np.reshape(container, (-1, len(cols)))
+cardio_features = ['HYPERTENSION_ONSET', 'HIGHCHOL_ONSET', 'HIGHCHOL','HYPERTENSION', 'CHEST_DISCOMFORT', 'CHEST_PAIN_30MIN',
+       'BREATH_SHORTNESS', 'DIAGNOSED_DIABETES','DIAGNOSED_PREDIABETES', 'RISK_DIABETES']
 
-    plt.figure(1, figsize=(12, 18))
-    sns.set(font_scale=1.0)
-    heat_map = sns.heatmap(distance_corr_matrix, cbar=False, annot=True, square=True, fmt='.2f',
-                   annot_kws = {'size': 10}, yticklabels=cols, xticklabels=cols)
+cardio_biochem_df = pd.concat([questionnaire_data[cardio_features], biochemistry_data], axis=1)
+
+print(cardio_biochem_df.describe())
+
+
+
+if True:
+
+    from scipy.stats import pointbiserialr
+
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    sns.reset_orig()
+
+    cols_bis = list(cardio_biochem_df.columns)
+
+    container_bis = len(cols_bis)*len(cols_bis)*[0.0]
+    for i in range(len(cols_bis)):
+        for j in range(i, len(cols_bis)):
+            m = i * len(cols_bis) + j
+            n = j * len(cols_bis) + i
+            container_bis[m] = pointbiserialr(cardio_biochem_df[cols_bis[i]], cardio_biochem_df[cols_bis[j]])[0]
+            container_bis[n] = container_bis[m]
+
+    biserial_corr_matrix = np.reshape(container_bis, (-1, len(cols_bis)))
+
+    plt.figure(1, figsize=(10, 8))
+    sns.set(font_scale=0.7)
+    heat_map_bis = sns.heatmap(biserial_corr_matrix, cbar=False, annot=True, square=True, fmt='.2f',
+                    annot_kws = {'size': 6}, yticklabels=cols_bis, xticklabels=cols_bis)
     plt.xticks(rotation='vertical')
     plt.yticks(rotation='horizontal')
     plt.tight_layout()
