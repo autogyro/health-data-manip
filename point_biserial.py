3734bda90a5a2f30e1c65c019f8a34dc8a212bfc
Juan E Rolon <rolon.math@gmail.com>
2018-03-12 22:26:05 -0500

    test putting two scatter plots together

--- point_biserial.py
+++ point_biserial.py
@@ -348,7 +348,16 @@ if False:
     cb.set_label('counts in bin')
     plt.show()
 
-#Generate a scatter plot using two continuous features
-if True:
-    biochemistry_data.plot(kind='scatter', x='LBXSATSI', y='LBXSASSI');
+
+#Test plotting two scatter plots together
+if False
+    plt.figure(1, figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
+    plt.scatter(biochemistry_data['LBXSATSI'].values, biochemistry_data['LBXSASSI'].values, c='orange',
+                alpha=0.3, edgecolors='brown')
+
+    plt.subplot(1, 2, 2)
+    plt.scatter(biochemistry_data['LBXSATSI'].values, biochemistry_data['LBXSCK'].values, c='green',
+                alpha=0.08, edgecolors='yellow')
     plt.show()
