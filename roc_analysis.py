a2eeb3e72c3146b33abc31b7374f17292ef34860
Juan E Rolon <rolon.math@gmail.com>
2018-04-18 14:54:37 -0500

    added oversampling feature

--- roc_analysis.py
+++ roc_analysis.py
@@ -12,7 +12,6 @@ mod_path = "/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capston
 txt = SourceFileLoader("text_utils", mod_path+"text_utils.py").load_module()
 gut = SourceFileLoader("graph_utils", mod_path+"graph_utils.py").load_module()
 
-
 #Path to datasets folders
 
 datasets_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/capstone/gits/health-data-manip/datasets/'
@@ -21,14 +20,26 @@ project_path = '/Users/juanerolon/Dropbox/_machine_learning/udacity_projects/cap
 full_data = pd.read_csv(project_path + 'nhanes_2013_2014_full_data.csv',index_col=0)
 
 data_cols = list(full_data.columns)
-bio_cols = []
-que_cols = []
+print("All labels in data:\n")
+print(data_cols)
+print("")
 
-for col in data_cols:
-    if ('LBX' in col):
-        bio_cols.append(col)
-    else:
-        que_cols.append(col)
+# Using all column labels in data
+if False:
+    bio_cols = []
+    que_cols = []
+
+    for col in data_cols:
+        if ('LBX' in col):
+            bio_cols.append(col)
+        else:
+            que_cols.append(col)
+
+#Selected column labels (see heat maps associations)
+
+bio_cols = ['LBXSGL']
+#bio_cols = ['LBXSGL', 'LBXSOSSI']
+
 
 biochemistry_data = full_data[bio_cols]
 
@@ -41,6 +52,10 @@ if True:
     #Model input features
     model_features = pd.concat([biochemistry_data, full_data.BMI], axis=1)
     model_features = pd.concat([model_features, full_data.AGE], axis=1)
+    model_features = pd.concat([model_features, full_data.HYPERTENSION_ONSET], axis=1)
+    model_features = pd.concat([model_features, full_data.HYPERTENSION], axis=1)
+    model_features = pd.concat([model_features, full_data.HIGHCHOL_ONSET], axis=1)
+    model_features = pd.concat([model_features, full_data.HIGHCHOL], axis=1)
     model_features = pd.concat([model_features, full_data.SMOKING], axis=1)
     model_features = pd.concat([model_features, full_data.ALCOHOL_NUM], axis=1)
     model_features = pd.concat([model_features, full_data.FAMILIAL_DIABETES], axis=1)
@@ -48,13 +63,17 @@ if True:
     model_features = pd.concat([model_features, full_data.GENDER_Female], axis=1)
     model_features = pd.concat([model_features, full_data.ETHNICITY_White], axis=1)
     model_features = pd.concat([model_features, full_data.ETHNICITY_Black], axis=1)
-    model_features = pd.concat([model_features, full_data.ETHNICITY_Hispanic], axis=1)
-    model_features = pd.concat([model_features, full_data.ETHNICITY_Asian], axis=1)
+    #model_features = pd.concat([model_features, full_data.ETHNICITY_Hispanic], axis=1)
+    #model_features = pd.concat([model_features, full_data.ETHNICITY_Asian], axis=1)
+
+
 
     #################################### TARGET #####################################
     #Target for prediction/classification
 
-    model_targets = full_data.DIAGNOSED_PREDIABETES
+
+    model_targets = full_data.DIAGNOSED_DIABETES
+    #model_targets = full_data.DIAGNOSED_PREDIABETES
     #model_targets = full_data.HIGHCHOL
     #model_targets = full_data.RISK_DIABETES
     #model_targets = full_data.HYPERTENSION
@@ -65,13 +84,131 @@ if True:
     print("No. of input features: {}".format(features_num))
 
 
+######################################################################################################
+################################# XGBOOST Model ######################################################
+######################################################################################################
+
+if True:
+    from xgboost import XGBClassifier
+
+    from sklearn.metrics import fbeta_score
+    from sklearn.metrics import f1_score
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import roc_auc_score
+    from sklearn.metrics import roc_curve, auc
+
+    seed = 7
+    np.random.seed(seed)
+
+    X0 = model_features.values
+    Y0 = model_targets.values
+
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X0, Y0, test_size=0.33, random_state=seed)
+
+    #=============================================================
+    #-------------------- Oversampling step ----------------------
+
+    from imblearn.over_sampling import SMOTE
+    from imblearn.over_sampling import RandomOverSampler
+
+    #Oversampling over entire dataset
+    if False:
+        X, Y = SMOTE(ratio='minority').fit_sample(X0, Y0)
+
+    # SMOTE Oversampling over the training dataset
+    if False:
+        X_train, y_train = SMOTE(ratio='minority',kind='regular',k_neighbors=3).fit_sample(X_train, y_train)
+
+    # Random Oversampling over the training dataset
+    if False:
+        X_train, y_train = RandomOverSampler(ratio='minority',random_state=0).fit_sample(X_train, y_train)
+
+
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+
+    predictions_prob_test  = model.predict(X_test)
+    predictions_test = [round(value) for value in predictions_prob_test]
+
+    predictions_prob_train = model.predict(X_train)
+    predictions_train = [round(value) for value in predictions_prob_train]
+
+    #Short test compare to CNN case
+    if False:
+        print(model.predict_proba(X_test))
+        print(model.predict_proba(X_test).shape)
+        sys.exit()
+
+    ############################# ROC_CURVE_XGBOOST ############################################
+    fpr, tpr, dash = roc_curve(y_test, model.predict_proba(X_test)[:, 1])
+    rocxg_df = pd.DataFrame({'fpr':fpr, 'tpr': tpr, 'dash': dash})
+    rocxg_df.to_csv('roc_curve_xgboost.csv')
+    ############################################################################################
+
+
+    #--------------------------------------
+    # Calculate the AUC
+    roc_auc = auc(fpr, tpr)
+    print('ROC AUC: %0.2f' % roc_auc)
+    #--------------------------------------
+
+    accuracy = accuracy_score(y_test, predictions_test)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+    pred_train_df = pd.concat([pd.DataFrame(predictions_prob_train, columns=['PROB']), pd.DataFrame(predictions_train, columns=['Y_PRED_TRAIN'])], axis=1)
+    pred_test_df  = pd.concat([pd.DataFrame(predictions_prob_test, columns=['PROB']), pd.DataFrame(predictions_test, columns=['Y_PRED_TEST'])], axis=1)
+
+    if False:
+        print("Predictions Preview:\n")
+        txt.headcounts(pred_train_df, 100)
+        txt.headcounts(pred_test_df,100)
+
+    #acc_train = accuracy_score(y_train, predictions_train)
+    #acc_test = accuracy_score(y_test, predictions_test)
+
+    #beta = 0.5
+
+    #fb_train = fbeta_score(y_train, predictions_train, beta=beta)
+    #fb_test = fbeta_score(y_test, predictions_test, beta=beta)
+
+    #f1_train = f1_score(y_train, predictions_train)
+    #f1_test = f1_score(y_test, predictions_test)
+
+    #roc_auc_train = roc_auc_score(y_train, predictions_train, average='micro')
+    #roc_auc_test = roc_auc_score(y_test, predictions_test, average='micro')
+
+    from sklearn.metrics import confusion_matrix
+
+    CM = confusion_matrix(y_test, predictions_test)
+    CML = np.array([['TN', 'FP'], ['FN', 'TP']])
+
+    #print("acc_train = {}, acc_test ={}".format(acc_train, acc_test))
+    print("Confusion Matrix:\n{}\n\n {} \n".format(CML, CM))
+    #print("f1_train = {}, f1_test ={}".format(f1_train, f1_test))
+    #print("fbeta_train = {}, fbeta_test ={}".format(fb_train, fb_test))
+    #print("ROC_AUC_train = {}, ROC_AUC_test ={}".format(roc_auc_train, roc_auc_test))
+
+
+    if False:
+        # Plot of a ROC curve for a specific class
+        plt.figure()
+        plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve - Hypertension Risk Prediction')
+        plt.legend(loc="lower right")
+        plt.show()
 
 #################################################################################################################
 ################################# Neural Network Model ############################################
 #################################################################################################################
 
 
-if True:
+if False:
 
     import tensorflow as tf
     from keras.models import Sequential
@@ -99,11 +236,12 @@ if True:
     #------Keras custom metrics--------
     import keras.backend as K
 
+    #----not working.....
     def k_custom_auc(y_true, y_pred):
 
         fpr, tpr, dash = roc_curve(y_true, y_pred)
         score = auc(fpr, tpr)
-        return  K.variable(score)
+        return K.variable(score)
 
     #----------------------------------
 
@@ -112,6 +250,11 @@ if True:
 
     X = model_features.values
     Y = model_targets.values
+
+    print(X.shape)
+    print(Y.shape)
+
+    sys.exit()
 
 
     from sklearn.model_selection import train_test_split
@@ -129,7 +272,7 @@ if True:
         model.add(Dense(16, activation='relu'))
         model.add(Dense(8, activation='relu'))
         model.add(Dense(1, activation='sigmoid'))
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy',k_custom_auc])
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
 
 
@@ -199,13 +342,14 @@ if True:
 
         #----------------------------------------------------------------------
         #select one of the models defined above and insert it as hyperparameter
-        model = KerasClassifier(build_fn=second_model, verbose=0)
+        #model = KerasClassifier(build_fn=second_model, verbose=0)
+        model = KerasClassifier(build_fn=initial_model, verbose=0)
         #-----------------------------------------------------------------------
 
         # define the grid search parameters
 
-        batch_size = [10, 20]   #batch_size = [10, 20, 40, 60, 80, 100]
-        epochs = [10, 50]       #epochs = [10, 50, 100]
+        batch_size = [10, 20, 40]   #batch_size = [10, 20, 40, 60, 80, 100]
+        epochs = [10, 50,100]       #epochs = [10, 50, 100]
 
         param_grid = dict(batch_size=batch_size, epochs=epochs)
 
@@ -309,100 +453,3 @@ if True:
     tf.Session().close()
 
 
-######################################################################################################
-################################# XGBOOST Model ######################################################
-######################################################################################################
-
-if False:
-    from xgboost import XGBClassifier
-
-    from sklearn.metrics import fbeta_score
-    from sklearn.metrics import f1_score
-    from sklearn.metrics import accuracy_score
-    from sklearn.metrics import roc_auc_score
-    from sklearn.metrics import roc_curve, auc
-
-    seed = 7
-    np.random.seed(seed)
-
-    X = model_features.values
-    Y = model_targets.values
-
-    from sklearn.model_selection import train_test_split
-
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=seed)
-
-    model = XGBClassifier()
-    model.fit(X_train, y_train)
-
-    predictions_prob_test  = model.predict(X_test)
-    predictions_test = [round(value) for value in predictions_prob_test]
-
-    predictions_prob_train = model.predict(X_train)
-    predictions_train = [round(value) for value in predictions_prob_train]
-
-    #Short test compare to CNN case
-    if False:
-        print(model.predict_proba(X_test))
-        print(model.predict_proba(X_test).shape)
-        sys.exit()
-
-    ############################# ROC_CURVE_XGBOOST ############################################
-    fpr, tpr, dash = roc_curve(y_test, model.predict_proba(X_test)[:, 1])
-    rocxg_df = pd.DataFrame({'fpr':fpr, 'tpr': tpr, 'dash': dash})
-    rocxg_df.to_csv('roc_curve_xgboost.csv')
-    ############################################################################################
-
-
-    #--------------------------------------
-    # Calculate the AUC
-    roc_auc = auc(fpr, tpr)
-    print('ROC AUC: %0.2f' % roc_auc)
-    #--------------------------------------
-
-    accuracy = accuracy_score(y_test, predictions_test)
-    print("Accuracy: %.2f%%" % (accuracy * 100.0))
-
-    pred_train_df = pd.concat([pd.DataFrame(predictions_prob_train, columns=['PROB']), pd.DataFrame(predictions_train, columns=['Y_PRED_TRAIN'])], axis=1)
-    pred_test_df  = pd.concat([pd.DataFrame(predictions_prob_test, columns=['PROB']), pd.DataFrame(predictions_test, columns=['Y_PRED_TEST'])], axis=1)
-
-    print("Predictions Preview:\n")
-    txt.headcounts(pred_train_df, 100)
-    txt.headcounts(pred_test_df,100)
-
-    acc_train = accuracy_score(y_train, predictions_train)
-    acc_test = accuracy_score(y_test, predictions_test)
-
-    beta = 0.5
-
-    fb_train = fbeta_score(y_train, predictions_train, beta=beta)
-    fb_test = fbeta_score(y_test, predictions_test, beta=beta)
-
-    f1_train = f1_score(y_train, predictions_train)
-    f1_test = f1_score(y_test, predictions_test)
-
-    roc_auc_train = roc_auc_score(y_train, predictions_train, average='micro')
-    roc_auc_test = roc_auc_score(y_test, predictions_test, average='micro')
-
-    from sklearn.metrics import confusion_matrix
-
-    CM = confusion_matrix(y_test, predictions_test)
-    CML = np.array([['TN', 'FP'], ['FN', 'TP']])
-
-    print("acc_train = {}, acc_test ={}".format(acc_train, acc_test))
-    print("Confusion Matrix:\n{}\n\n {} \n".format(CML, CM))
-    print("f1_train = {}, f1_test ={}".format(f1_train, f1_test))
-    print("fbeta_train = {}, fbeta_test ={}".format(fb_train, fb_test))
-    print("ROC_AUC_train = {}, ROC_AUC_test ={}".format(roc_auc_train, roc_auc_test))
-
-    # Plot of a ROC curve for a specific class
-    plt.figure()
-    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('ROC Curve - Hypertension Risk Prediction')
-    plt.legend(loc="lower right")
-    plt.show()
